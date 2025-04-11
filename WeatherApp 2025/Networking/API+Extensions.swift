fa906fb946d43e180d00894c9ed39e00a597cfcd
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-04-12 01:20:31 +0200

    NetworkManager

---
+++ WeatherApp 2025/Networking/API+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  API+Extensions.swift
+//  WeatherApp 2025
+//
+//  Created by Manyuchi, Carrington C on 2025/04/12.
+//
+
+import Foundation
+
+extension API {
+    static let baseURLString = "https://api.openweathermap.org/data/2.5/"
+    
+    static func getURLFor(lat: Double, lon: Double) -> String {
+        return "\(baseURLString)oncall?lat=\(lat)&lon=\(lon)&exclude=minutely&appid=\(key)&units=metric"
+    }
+    
+    
+}
---
+++ WeatherApp 2025/Networking/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  WeatherApp 2025
+//
+//  Created by Manyuchi, Carrington C on 2025/04/12.
+//
+
+import Foundation
+
+struct API {
+    static let key = ""
+}
