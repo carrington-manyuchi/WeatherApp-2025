5e4ee91e9677d0703f38989697d095415b909bd7
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-04-12 00:43:18 +0200

    Initial Commit

---
+++ WeatherApp 2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherApp 2025
+//
+//  Created by Manyuchi, Carrington C on 2025/04/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherApp 2025/WeatherApp_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_2025App.swift
+//  WeatherApp 2025
+//
+//  Created by Manyuchi, Carrington C on 2025/04/12.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
